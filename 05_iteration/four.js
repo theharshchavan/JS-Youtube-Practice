6bdb26d82a5ba05039afd2da21f576348294e8ba
theharshchavan <harshchavan6362@gmail.com>
2024-09-13 21:51:19 +0530

    add files

---
+++ 05_iteration/four.js
@@ -0,0 +1,12 @@
+const myObject = {
+    js : 'javascript',
+    cpp : 'c++',
+    rb : 'ruby',
+    py : 'python'
+}
+
+for (const key in myObject) {
+    console.log(myObject[key]);
+}
+
+console.log(myObject);
