dc3b7d8f2f520c09f9a832391d3aa223aa50bd34
theharshchavan <harshchavan6362@gmail.com>
2024-09-15 21:30:06 +0530

    code change

--- 07_projects/BMIcalculator/app.js
+++ 07_projects/BMIcalculator/app.js
@@ -6,17 +6,23 @@ form.addEventListener("submit", function(e){
     const height = parseInt(document.getElementById('height').value)
     const weight = parseInt(document.getElementById('weight').value)
     const result = document.getElementById('results')
+    const msg = document.getElementById('msg')
 
     if(height === '' || height < 0 || isNaN(height)){
         result.innerHTML = `please give a valid height ${height}`
+        msg.innerHTML = ''
     } else if(weight === '' || weight < 0 || isNaN(weight)){
         result.innerHTML = `please give a valid height ${weight}`
+        msg.innerHTML = ''
     } else {
         const value = (weight / (height*height/ 10000)).toFixed(2)
         result.innerHTML = `<span>${value}</span>`
+        msg.innerHTML = ''
+    
+        
+        
 
         if(value < 18.6){
-            const msg = document.getElementById('msg')
             msg.innerHTML = `under weight`
         } else if(value > 18.6 && value < 24.9){
             msg.innerHTML = `Normal range`
