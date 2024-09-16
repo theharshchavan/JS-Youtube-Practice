84c8de8bce984b24b4c6da2e0adf9572f87714d2
theharshchavan <harshchavan6362@gmail.com>
2024-09-16 11:31:54 +0530

    update code

--- 07_projects/GuessTheNumber/app.js
+++
@@ -1,8 +0,0 @@
-const randomNum = parseInt(Math.random()*100+1)
-
-const guessInput = document.getElementById('guessField')
-const submit = document.getElementById('subt')
-const previousguess = document.getElementsByClassName('guesses')
-const remaining = document.getElementsByClassName('lastResult')
-const lohi = document.getElementsByClassName('lowOrHi')
-const start = document.getElementsByClassName('resultParas')
